7db522683747afe967b0c49b3ce22b74877fb6cb
Shane Stephenson <stephenson.shane.a@gmail.com>
2023-04-05 22:46:34 -0400

    updated version

--- examples/hello_world/target/debug/deps/hello_world-a3a1b2b8147cac1e.d
+++
@@ -1,5 +0,0 @@
-/home/oatbread/Projects/bob/examples/hello_world/target/debug/deps/hello_world-a3a1b2b8147cac1e.rmeta: src/main.rs
-
-/home/oatbread/Projects/bob/examples/hello_world/target/debug/deps/hello_world-a3a1b2b8147cac1e.d: src/main.rs
-
-src/main.rs:
