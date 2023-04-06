11ae02432aef932fb94fda000ce1eebb6e60e7a2
Shane Stephenson <stephenson.shane.a@gmail.com>
2023-04-05 22:49:04 -0400

    moved everything from bob.rs to lib.rs

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,4 +1,4 @@
-use bob_the::bob::Script;
+use bob_the::Script;
 
 
 fn main() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,32 @@
 pub mod interpreter;
 pub mod lexer;
 pub mod parser;
-pub mod bob;
+
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+
+pub struct Script {
+    source: String,
+}
+
+impl Script {
+    pub fn new(file: &str) -> Result<Self, std::io::Error> {
+        let mut file = fs::File::open(Path::new(file))?;
+        let mut source = String::new();
+        file.read_to_string(&mut source)?;
+        Ok(Script { source })
+    }
+
+    pub fn from(source: &str) -> Self {
+        Script {
+            source: source.to_string(),
+        }
+    }
+
+    pub fn run(&self) -> Result<(), interpreter::InterpreterError> {
+        let tokens = lexer::tokenize(&self.source)?;
+        let ast = parser::parse(tokens)?;
+        interpreter::Interpreter::new().execute(&ast)
+    }
+}
